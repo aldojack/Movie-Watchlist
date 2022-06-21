432ed9c2e2eef3f1d440981ed3b6617fa72be787
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-06-21 21:48:10 +0100

    local storage and refactored to create each movie result

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+
+import {createElement} from './utillity.js';
 
 const api = '1f5fb779';
 let searchValue = '';
 const searchField = document.getElementById('movieSearch');
 const searchBtn = document.getElementById("SearchBtn");    
 const resultsContainer = document.getElementById('results');
-const results = [];
+let localWatchlist = [];
+const storedLocalStorage = JSON.parse(localStorage.getItem("watchlist"))
+
+if (storedLocalStorage) {
+    localWatchlist = storedLocalStorage;
+}
+/*
+Local storange clearing after refresh
+const locStorage = localStorage.getItem('watchlist');
+let localWatchlist;
+
+localStorage ? localWatchlist = locStorage : localWatchlist = [];
+*/
 
 const setValue = (event) => {
     searchValue = event.target.value.toLowerCase();
@@ -13,29 +26,69 @@ const setValue = (event) => {
     {
         searchMovie();
     }
-    console.log(searchValue)
 }
 
-const searchMovie = async () => {
-    const res = await fetch(`https://www.omdbapi.com/?apikey=${api}&s=${searchValue}`);
-    const {Response, Search} = await res.json();
-    for(let movie of Search)
-    {
-        console.log(movie)
+    const searchMovie = async () => {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${api}&s=${searchValue}`);
+        const { Response, Search } = await res.json();
+
+        //If results found then do this
+        if (Response) {
+            console.log("Search results: ", Search);
+
+            for (let movie of Search) {
+                getMovie(movie);
+            }
+        }
+        //Else print the results to screen
+        else {
+            console.log("No result found")
+        }
     }
-    //If no search found then do this
-    if(!Response)
-    {
-        console.log("No result found")
-    }
-    //Else print the results to screen
-    else{
-        console.log(Search);
-    }
-}    
+    
+const getMovie = async ({imdbID}) => {
+    const res = await fetch(`https://www.omdbapi.com/?apikey=${api}&i=${imdbID}&plot=full`);
+    const { Title, Runtime, Genre, imdbRating, Plot, Poster } = await res.json();
+
+    //Create the movie and add to the DOM
+    //Create the container that will hold the film result
+    const movieContainer = createElement('div', { class: ['movie-container', 'flex'] });
+    resultsContainer.appendChild(movieContainer);
+
+    //Create a div for the poster
+    const posterDiv = createElement('div', { class: ['movie-poster'] });
+    const image = createElement('img');
+    //If movie has a poster then show the default if not then show placeholder image
+    Poster != 'N/A' ? image.src = Poster : image.src = './images/no-poster.jpg';
+    posterDiv.appendChild(image);
+
+    const movieInfoDiv = createElement('div', { class: ['movie-information', 'flex', 'flex-col'] });
+
+    const movieTitle = createElement('h3', { class: ['movie-title'], text: Title });
+    const rating = createElement('span', { class: ['rating'], text: `⭐ ${imdbRating}` })
+    movieTitle.appendChild(rating);
+    const movieAddInfo = createElement('div', { class: ['movie-additional', 'flex'] });
+    const runTime = createElement('p', { class: ['runtime'], text: Runtime });
+    const genre = createElement('p', { class: ['genre'], text: Genre });
+    const watchlist = createElement('div', { class: ['watchlist', 'flex'] });
+    const icon = createElement('i', { class: ['fa-solid', 'fa-circle-plus', 'watchlist'], role: 'button', value: Title });
+    const span = createElement('p', { text: "Watchlist" });
+    watchlist.append(icon, span);
+    const moviePlot = createElement('p', { class: ['movie-desc'], text: Plot})
+    
+    icon.addEventListener('click', (event) => {
+        console.log(event);
+        localWatchlist.push(Title);
+        localStorage.setItem("watchlist", JSON.stringify(localWatchlist))
+        console.log(localStorage.getItem('watchlist'))
+    })
+    movieAddInfo.append(runTime,genre, watchlist)
+    movieInfoDiv.append(movieTitle,movieAddInfo, moviePlot);
+
+    movieContainer.append(posterDiv,movieInfoDiv); 
+
+}
+
+
 searchField.addEventListener('keyup', setValue);
 searchBtn.addEventListener('click', searchMovie);
-
-
-
-})
